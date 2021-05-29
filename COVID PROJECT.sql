dabb6502e3fe60ab006c26e84a538d4f8ee44ac2
Joy Onana <84628487+joy-onana@users.noreply.github.com>
2021-05-29 09:46:15 +0100

    Update COVID PROJECT.sql

--- COVID PROJECT.sql
+++ COVID PROJECT.sql
@@ -33,7 +33,7 @@ and cast( date as date) >= '2021-03-01'
 group by location, population,DATENAME(mm,cast( date as date))
 
 
---------day on day vaccinated growth (Nigeria as case study)
+--------previous day figure (Nigeria as case study)
 with o ( date_,location,total_cases,vaccinations,population,percentage_vaccinated)
 as   
 (
@@ -52,3 +52,25 @@ round((percentage_vaccinated/ LAG(percentage_vaccinated) OVER (ORDER BY date_) -
 round((vaccinations/ Lag(vaccinations) OVER (ORDER BY date_) - 1),2) as vac_growth2
 from o
 order by date_ asc
+
+--------day on day growth (Nigeria as case study)
+with all_vac
+as 
+(select date,
+location, total_cases,
+max(cast(people_vaccinated as int)) as curent_vaccinations,
+lag(max(cast(people_vaccinated as int))) OVER (ORDER BY date - 1) as previous_vaccinations
+,population,
+round((cast(people_vaccinated as int))/population*100,2) as percentage_vaccinated,
+LAG(max((cast(people_vaccinated as int))/population*100)) OVER (ORDER BY date - 1) as previous_per_vaccinated
+from dbo.['project portfolio$']
+where location = 'Nigeria'
+and cast( date as date) >= '2021-03-01'
+group by location, population,date,total_cases,
+cast(people_vaccinated as int),
+round((cast(people_vaccinated as int))/population*100,2))
+select all_vac.*, 
+(curent_vaccinations) - (previous_vaccinations)/(previous_vaccinations) as growth,
+(curent_vaccinations) - (previous_vaccinations)/(previous_vaccinations) * 100 as per_growth
+from all_vac
+order by date desc
