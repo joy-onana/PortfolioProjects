1b86d64c6bdc6bdc4eba50fed114dc5f395db0b6
Joy Onana <84628487+joy-onana@users.noreply.github.com>
2022-07-02 07:24:00 +0100

    Update COVID PROJECT.sql

--- COVID PROJECT.sql
+++ COVID PROJECT.sql
@@ -34,7 +34,7 @@ group by location, population,DATENAME(mm,cast( date as date))
 
 
 --------previous day figure (Nigeria as case study)
-with o ( date_,location,total_cases,vaccinations,population,percentage_vaccinated)
+with Nigeria_data ( date_,location,total_cases,vaccinations,population,percentage_vaccinated)
 as   
 (
 select cast( date as date) as date_,
@@ -47,7 +47,7 @@ where location = 'Nigeria'
 and cast( date as date) >= '2021-03-01'
 group by location, population,cast( date as date),total_cases,cast(people_vaccinated as int)
 )
-select o.*, 
+select Nigeria_data.*, 
 round((percentage_vaccinated/ LAG(percentage_vaccinated) OVER (ORDER BY date_) - 1),2) as vac_growth,
 round((vaccinations/ Lag(vaccinations) OVER (ORDER BY date_) - 1),2) as vac_growth2
 from o
@@ -81,7 +81,8 @@ order by date desc
 -----FINAL DATA SCRIPTING FOR PERSONAL PROJECT
 select date,
 location, total_cases,new_cases,cast(total_deaths as int) as total_deaths,
-max(cast(people_vaccinated as int)) as curent_vaccinations, cast(people_fully_vaccinated as int) as fully_vaccinated,
+max(cast(people_vaccinated as int)) as curent_vaccinations, 
+cast(people_fully_vaccinated as int) as fully_vaccinated,
 lag(max(cast(people_vaccinated as int))) OVER (ORDER BY date - 1) as previous_vaccinations
 ,population,
 round((cast(people_vaccinated as int))/population*100,2) as percentage_vaccinated,
